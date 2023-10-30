3bce92ea34609b0ccf3c747812fbd011a8db0d58
yamanidev <elamine.yamani@gmail.com>
2023-10-30 12:12:13 +0100

    Extended color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: {
+				"ghost-white": "#F7F9FC"
+			},
 			fontFamily: {
 				alexandria: ['Alexandria', 'sans-serif']
 			},
