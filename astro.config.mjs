3fb11547bfbf5f6ab194ce882a17ac765d7532e4
yamanidev <elamine.yamani@gmail.com>
2023-10-30 16:56:35 +0100

    Replaced Preact with React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import react from "@astrojs/react";
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),preact()]
+  integrations: [tailwind(), react()]
 });
