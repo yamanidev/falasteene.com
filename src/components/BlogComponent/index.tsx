beca6190b32be724325995d6651ecba4ba99d29c
Bahaa-eddine-MB <bm.bouzeboudja@esi-sba.dz>
2023-10-31 16:51:53 +0100

    news blogs respo

--- src/components/BlogComponent/index.tsx
+++ src/components/BlogComponent/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useLayoutEffect, useRef } from 'react';
 
 interface Props {
   link: string;
@@ -8,26 +8,45 @@ interface Props {
 }
 
 function BlogComponent({ link, text, time, last }: Props) {
+  const [blogComponentHeight, setBlogComponentHeight] = useState<number | null>(null);
+  const elementRef = useRef<HTMLAnchorElement | null>(null);
+
   function processTime(time: string): string {
     const indexOfSecondمنذ = time.indexOf('منذ', time.indexOf('منذ') + 1);
     if (indexOfSecondمنذ !== -1) {
-      return time.substring(0, indexOfSecondمنذ).trim();
+      return `${time.substring(0, indexOfSecondمنذ).trim()}:`;
     }
-    return time;
+    return `${time}:`;
   }
+  useLayoutEffect(() => {
+    const calculateHeight = () => {
+      if (elementRef.current) {
+        setBlogComponentHeight(elementRef.current.clientHeight);
+      }
+    };
+    calculateHeight();
+    setTimeout(calculateHeight, 0);
+    window.addEventListener('resize', calculateHeight);
+    return () => {
+      window.removeEventListener('resize', calculateHeight);
+    };
+  }, []);
   return (
-    <a href={link} target="_blank" className="flex items-center gap-4">
-      <div className="relative">
-        <div className="relative h-6 w-6 rounded-full border-2 border-mainBlue">
-          <div className="absolute right-[50%] top-[50%] h-4 w-4  translate-x-[50%] translate-y-[-50%]  rounded-full bg-mainBlue "></div>
+    <div className='flex flex-col'>
+      <a ref={elementRef} href={link} target="_blank" className="flex items-center gap-4 ">
+        <div className="relative">
+          <div className="relative h-6 w-6 rounded-full border-2 border-mainBlue">
+            <div className="absolute right-[50%] top-[50%] h-4 w-4  translate-x-[50%] translate-y-[-50%]  rounded-full bg-mainBlue "></div>
+          </div>
         </div>
-        {!last && (
-          <div className="absolute right-[50%] top-6 h-[33px] w-[2px] translate-x-[50%] bg-disabledText"></div>
-        )}
-      </div>
-      <p className="text-disabledText">{processTime(time)} :</p>
-      <p className="font-bold text-mainText">{text}</p>
-    </a>
+        <p className="text-xs text-disabledText sm:text-sm md:text-base">{processTime(time)}</p>
+        <p className="text-xs font-bold text-mainText sm:text-sm md:text-base">{text}</p>
+      </a>
+      {!last && (
+        <div
+          className={` mr-[11px] w-[2px] h-8  bg-disabledText`}></div>
+      )}
+    </div>
   );
 }
 
--- src/components/NewsComponent/index.tsx
+++ src/components/NewsComponent/index.tsx
@@ -22,8 +22,6 @@ function NewsComponent() {
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-
         setHeadline(data.headline);
         setLiveBlogUpdates(data.blogs);
       })
@@ -34,10 +32,12 @@ function NewsComponent() {
 
   return (
     <div>
-      <div className="px-4 py-10">
+      <div className="px-2 py-10 lg:px-4">
         {headline && (
-          <div className='relative w-[95%]'>
-            <a className="text-5xl font-bold leading-normal text-mainText " href={headline.link}>
+          <div className="relative w-[95%]">
+            <a
+              className="text-xl font-bold text-mainText sm:text-2xl md:text-3xl lg:text-5xl lg:leading-normal "
+              href={headline.link}>
               {headline.title}
             </a>
             <img
@@ -48,17 +48,16 @@ function NewsComponent() {
           </div>
         )}
       </div>
-      <ul className=" flex flex-col gap-8">
+      <ul className=" flex flex-col">
         {liveBlogUpdates &&
           liveBlogUpdates.map((update, index) => (
-            <li key={index}>
-              <BlogComponent
-                link={update.link}
-                text={update.content}
-                time={update.time}
-                last={liveBlogUpdates.length - 1 == index}
-              />
-            </li>
+            <BlogComponent
+              key={index}
+              link={update.link}
+              text={update.content}
+              time={update.time}
+              last={liveBlogUpdates.length - 1 == index}
+            />
           ))}
       </ul>
     </div>
