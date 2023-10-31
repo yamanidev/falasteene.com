ee3d617ac254faa5e2b263a9e002c8c0e20d37a9
Bahaa-eddine-MB <bm.bouzeboudja@esi-sba.dz>
2023-10-31 20:38:44 +0100

    news page is done

--- src/components/SingleNewsComponent/index.tsx
+++ src/components/SingleNewsComponent/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 function SingleNewsComponent({ link, title }: Props) {
   return (
     <a href={link} target="_blank" className="flex items-center gap-2 rounded-md border-2 p-4">
-      <img className="w-[80px] md:w-[100px] lg:w-[120px]" alt="gaza" src="/assets/news/gaza.svg" />
+      <img className="w-[80px] lg:w-[100px] md:w-[120px]" alt="gaza" src="/assets/news/gaza.svg" />
       <div className="flex flex-col gap-2">
         <span className="self-start rounded-lg bg-mainBlue px-2 text-xs text-white  sm:text-sm">
           غزة الآن - Gaza Now
