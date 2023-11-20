69960ddb70ca4ba31a3420ef8100657714c58d46
yamanidev <elamine.yamani@gmail.com>
2023-11-20 14:49:14 +0100

    Added check icon

---
+++ src/components/icons/CheckIcon.tsx
@@ -0,0 +1,16 @@
+function CheckIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      className={className}
+      viewBox="0 0 24 24">
+      <path
+        d="M10.0007 15.1709L19.1931 5.97852L20.6073 7.39273L10.0007 17.9993L3.63672 11.6354L5.05093 10.2212L10.0007 15.1709Z"
+        fill="#009736"></path>
+    </svg>
+  );
+}
+
+export default CheckIcon;
