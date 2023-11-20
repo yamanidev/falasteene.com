546365243444e31af0e0962777947c676323b724
yamanidev <elamine.yamani@gmail.com>
2023-11-20 14:51:00 +0100

    Added hook

---
+++ src/hooks/useCopyToClipboard.ts
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from 'react';
+
+// From: https://www.benmvp.com/blog/copy-to-clipboard-react-custom-hook/
+
+type CopyStatus = 'inactive' | 'copied' | 'failed';
+
+function useCopyToClipboard(text: string, notifyTimeout = 2500) {
+  const [copyStatus, setCopyStatus] = useState<CopyStatus>('inactive');
+  const copy = useCallback(() => {
+    navigator.clipboard.writeText(text).then(
+      () => setCopyStatus('copied'),
+      () => setCopyStatus('failed')
+    );
+  }, [text]);
+
+  useEffect(() => {
+    if (copyStatus === 'inactive') {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => setCopyStatus('inactive'), notifyTimeout);
+
+    return () => clearTimeout(timeoutId);
+  }, [copyStatus]);
+
+  return [copyStatus, copy];
+}
+
+export default useCopyToClipboard;
