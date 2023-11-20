92ee32edd0682ea279a306892a4d2aa57cfdbb23
yamanidev <elamine.yamani@gmail.com>
2023-11-20 11:27:37 +0100

    Formatted file + added hover only when supported

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"ghost-white": "#F7F9FC",
-				"anti-flash-white": "#F1F1F1",
-				"palestine-red": "#EE2A35",
-				"palestine-green": "#009736"
-			},
-			fontFamily: {
-				alexandria: ['Alexandria', 'sans-serif']
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'ghost-white': '#F7F9FC',
+        'anti-flash-white': '#F1F1F1',
+        'palestine-red': '#EE2A35',
+        'palestine-green': '#009736'
+      },
+      fontFamily: {
+        alexandria: ['Alexandria', 'sans-serif']
+      }
+    }
+  },
+  future: {
+    hoverOnlyWhenSupported: true
+  },
+  plugins: []
+};
