8957c8d63d242409243b848d05b9587c2803a4aa
yamanidev <elamine.yamani@gmail.com>
2024-05-06 21:18:10 +0100

    Added site to defineConfig()

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://falasteene.com/",
   integrations: [tailwind(), react()]
 });
